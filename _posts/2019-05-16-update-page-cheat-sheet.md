569d43acb6cc28d0bb2d0bd05240294f042b04b9
Henry Bernreuter <42005962+HenryBernreuter@users.noreply.github.com>
2019-05-16 15:19:25 -0400

    update cheatsheet

---
+++ _posts/2019-05-16-update-page-cheat-sheet.md
@@ -0,0 +1,59 @@
+---
+layout: posts
+title: Update Page Cheat Sheet
+date: 2019-05-16 14:33 -0400
+tags: [data wrangling, data science, data analytics]
+header:
+  image: "/images/perceptron/percept.jpg"
+excerpt: "Data Wrangling, Data Science, Data Visualizations"
+mathjax: "true"
+---
+# Update Process Notes
+Open command line
+
+cd desktop
+
+cd file folder
+
+bundle exec jekyll post "post name"
+
+run preview server
+
+bundle exec jekyll serve
+
+## Front matter properties
+[link](https://jekyllrb.com/docs/front-matter/)
+
+
+When done with changes
+
+open pathway on command line
+cd desktop
+
+cd file folder
+
+git status
+
+git add - -all
+
+git commit -m "name of post"
+
+### Add a Page
+Open command line
+
+cd desktop
+
+cd file folder
+
+bundle exec jekyll page "projects"
+
+### Publish to the web
+ Open command line
+
+ cd desktop
+
+ cd file folder
+
+ bundle exec jekyll build
+
+ 
--- public/assets/js/lunr/lunr-store.js
+++ public/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"H1 HeadingH2 HeadingH3 HeadingHere’s some basic text.And here’s some italicsHere’s some bold text.What about a link?Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y) return zR code block:library(tidyverse)df &lt;- read_csv(\"some_file.csv\")head(df)Here’s some...","categories": [],
         "tags": ["data wrangling","data science","messy data"],
         "url": "http://localhost:4000/perceptron/",
+        "teaser":null},{
+        "title": "Update Page Cheat Sheet",
+        "excerpt":"Update Process NotesOpen command linecd desktopcd file folderbundle exec jekyll post “post name”run preview serverbundle exec jekyll serveFront matter propertieslinkWhen done with changesopen pathway on command linecd desktopcd file foldergit statusgit add - -allgit commit -m “name of post”Add a PageOpen command linecd desktopcd file folderbundle exec jekyll page “projects”Publish...","categories": [],
+        "tags": ["data wrangling","data science","data analytics"],
+        "url": "http://localhost:4000/update-page-cheat-sheet/",
         "teaser":null}]
