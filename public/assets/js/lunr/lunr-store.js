5dcd2bca94f8b733991c449a2d526033ad3acc49
Henry Bernreuter <42005962+HenryBernreuter@users.noreply.github.com>
2019-07-14 17:02:56 -0400

    New Resume

--- public/assets/js/lunr/lunr-store.js
+++ public/assets/js/lunr/lunr-store.js
@@ -1,26 +1,26 @@
 var store = [{
         "title": "Resume",
-        "excerpt":"=========================Education 2014-2019 B.I.S GAME DESIGN, GEORGIA STATE UNIVERSITYGPA: 3.25Experience 4/1/2019 – PRESENT AWS SOLUTIONS ENGINEER, ROBINSON GAMING INDUSTRIES Team Leader in migration of media clients to cloud servers. 1/1/2018 – 4/1/2019 DATA ANALIST, ROBINSON GAMING INDUSRTRIES Developed statistical insights for gaming companies. 3/26/2008 – 4/15/2016 FOUNDER, DISTURBED MEDIA LLC Developed...","categories": [],
+        "excerpt":"=========================Education 2014-2019 Bachelor of Interdisciplinary Studies, concentration in Game Design an Development Georgia State UniversityGPA: 3.25 ExperienceDisturbed Media:Tampa Bay, Florida Launched immersive customer experiences across digital, mobile, and store touch-points &amp; developing next-generation customer journey/contextual marketing solutions Prepared sales activity and performance reports Reduced response time for customer issuesRobinson Gaming...","categories": [],
         "tags": ["Henry Bernreuter"],
-        "url": "/resume/",
+        "url": "http://localhost:4000/resume/",
         "teaser":null},{
         "title": "Kaggle Titanic Solution In Python",
         "excerpt":"titanic-solution Titanic: Machine Learning from Disaster&#182;Predict survival on the Titanic&#182;Defining the problem statementCollecting the dataExploratory data analysisFeature engineeringModellingTesting1. Defining the problem statement&#182;Complete the analysis of what sorts of people were likely to survive.In particular, we ask you to apply the tools of machine learning to predict which passengers survived the...","categories": [],
         "tags": [],
-        "url": "/kaggle-titanic-solution-in-python/",
+        "url": "http://localhost:4000/kaggle-titanic-solution-in-python/",
         "teaser":null},{
         "title": "Facebook Analytics using ggplot in Python",
         "excerpt":"Project 1 CTW Directed Readings 4999Using data from Facebook group create data visualization similar to ggplot2 but using Python 31.Import data using pandas. Facebook downloads data as a .xls. In order to use the data it must be cleaned and merged into a .csv file format.import pandas as pdimport numpy...","categories": [],
         "tags": ["python ggplot","python in r","data analytics"],
-        "url": "/facebook-analytics-using-ggplot-in-python/",
+        "url": "http://localhost:4000/facebook-analytics-using-ggplot-in-python/",
         "teaser":null},{
         "title": "Python in One Post",
         "excerpt":"Python in One Post This tutorial is for anyone that is a beginner to programming.It is designed to help you get started immeidately working on python projects. It is not intended as a complete course. Good Luck, Remember first rule in programming:Don't Panic&#182;If you would like me to make more...","categories": [],
         "tags": [],
-        "url": "/python-in-one-post/",
+        "url": "http://localhost:4000/python-in-one-post/",
         "teaser":null},{
         "title": "Cool Data Visualizations in Python (code Included)",
         "excerpt":"Cool Data Visualizations in Python (code included). Support my efforts and see my story at the bottom of the page.&#182;In&nbsp;[23]: # Importing libsimport seaborn as snsimport matplotlib.pyplot as pltfrom scipy.stats import skewnorm# Create the dataspeed = skewnorm.rvs(4, size=50)size = skewnorm.rvs(4, size=50)# Create and show the 2D Density plotax = sns.kdeplot(speed,...","categories": [],
         "tags": ["Henry Bernreuter"],
-        "url": "/cool-data-visualizations-in-python-code-included/",
+        "url": "http://localhost:4000/cool-data-visualizations-in-python-code-included/",
         "teaser":null}]
